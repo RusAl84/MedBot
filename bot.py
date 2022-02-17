428544b4638198c95a2e3effb48325b945cdebb8
Aleksey <rusal@bk.ru>
2022-02-17 23:54:33 +0300

    fix

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 ﻿from webbrowser import get
 import telebot;
 
-
-bot = telebot.TeleBot('5111904045:AAEDPZLqKaacz7BFQV9Aohjj-5gmjEmoUCA');
+bot = telebot.TeleBot('5111904045:AAH8VIo6dXf1Uz-CddZ818PUOWXzoF0j6t8');
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -11,7 +10,7 @@ def get_text_messages(message):
     if str(message.text).lower() == "привет":
         bot.send_message(message.from_user.id, helpStr)
     elif str(message.text).lower() == "/help":
-        bot.send_message(message.from_user.id, "MedBot - Многофункциональный сервис для медицинской диагностики заболеваний. Внимание: при любом диагнозе необходима консультация врача! \n Список команд: \n /q - Начать опрос\n")
+        bot.send_message(message.from_user.id, "MedBot - Многофункциональный сервис для медицинской диагностики заболеваний. Внимание: при любом диагнозе необходима консультация врача! \n Список команд: \n /q - Начать опрос\n /a число от 0 до 100 в % насколько верно утверждение\n")
     elif str(message.text).lower() == "/q":
         bot.send_message(message.from_user.id, "dds")
     else:
